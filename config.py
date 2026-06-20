a7fa8cb11b08f514786207ce86096ee9ad217108
derick871 <wanyamaderrick4@gmail.com>
2026-06-20 16:17:44 +0300

    update config file:add a api_key

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY= os.getenv("CRYPTO_API_KEY")
+BASE_URL = "https://min-api.cryptocompare.com/data/price"
+CRYPTO_TICKER= "BITCOIN"
+FIAT_CURRENCY= "USD"
+if __name__== "__main__":
+    print("api fetched successfuly")
+
--- notifier.py
+++ notifier.py
@@ -1,26 +1,21 @@
 import sys
-#import os
+import os
 from plyer import notification
 
 def send_alert(title, message):
-    """
-    Dispatches a cross-platform desktop notification banner, triggers an audible 
-    system bell, and prints a formatted log to the standard output.
-    """
+   
     try:
-        # Corrected method: notification.notify()
         notification.notify(
             title=title,
             message=message,
             app_name="Crypto Alert Bot",
-            timeout=6  # Duration (in seconds) the notification stays on screen
+            timeout=10  # Duration (in seconds) the notification stays on screen
         )
         
         # Trigger an audible system alert bell
         sys.stdout.write("\a")
         sys.stdout.flush()
         
-        # Log a clean, standardized console message
         print(f"\n[ALERT NOTIFIED] {title} — {message}")
         
     except Exception as e:
@@ -31,6 +26,6 @@ if __name__ == "__main__":
     # allows you to test the file independently by running: python notifier.py
     print("Testing notification dispatch system...")
     send_alert(
-        title=" BTC Target Ceiling Smashed!", 
+        title=" BITCOIN Target Ceiling Smashed!", 
         message="Bitcoin has surpassed your alert threshold of $105,000.00."
     )
